ccf34fb1b0303f87e45c101a2d7a32d5f7ba1e18
coji <coji@techtalk.jp>
2023-09-25 10:20:16 +0900

    fix: parse

--- setup.ts
+++ setup.ts
@@ -1,4 +1,4 @@
 import { createDatabase } from '~/service/database'
 
 const { db } = createDatabase()
-db.run(`CREATE TABLE IF NOT EXISTS messages(message TEXT)`)
+db.run(`CREATE TABLE IF NOT EXISTS messages(message TEXT, created_at DATETIME DEFAULT CURRENT_TIMESTAMP)`)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,18 +4,20 @@ import type { ActionFunctionArgs, LoaderData } from '~/interfaces'
 import { createDatabase } from '~/service/database'
 
 const schema = z.object({
-  message: z.string().min(0),
+  message: z.string().nonempty('Please enter a message'),
 })
 
 export const loader = () => {
   const { db } = createDatabase()
-  const messages = (db.query('SELECT message FROM messages').all() as { message: string }[]) || []
+  const messages =
+    (db.query('SELECT message, created_at FROM messages').all() as { message: string; created_at: string }[]) || []
   return { messages }
 }
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   const submission = Object.fromEntries((await request.formData()).entries())
   const { message } = schema.parse(submission)
+  console.log({ message })
   const { db } = createDatabase()
   db.run('INSERT INTO messages (message) VALUES (?)', [message])
   return {}
@@ -23,7 +25,7 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 
 const IndexPage = ({ loaderData: { messages } }: { loaderData: LoaderData<typeof loader> }) => {
   return (
-    <div className="container grid h-screen grid-cols-1 grid-rows-[auto_1fr_auto] justify-items-center gap-4">
+    <div className="mx-auto grid h-screen max-w-xl grid-cols-1 grid-rows-[auto_1fr_auto] gap-4">
       <header className="text-4xl">Hello bun!</header>
 
       <main>
@@ -33,13 +35,15 @@ const IndexPage = ({ loaderData: { messages } }: { loaderData: LoaderData<typeof
             <Input id="message" type="text" name="message" />
           </fieldset>
           <Button>POST!</Button>
-        </form>
 
-        <Stack>
-          {messages.map((message) => (
-            <div>{message.message}</div>
-          ))}
-        </Stack>
+          <Stack>
+            {messages.map((message, idx) => (
+              <div key={idx}>
+                {message.message} {message.created_at}
+              </div>
+            ))}
+          </Stack>
+        </form>
       </main>
 
       <footer>Copyright &copy; {new Date().getFullYear()} coji.</footer>
